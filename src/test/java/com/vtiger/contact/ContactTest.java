8184e9b899c00fac8db28e1032dfb709346335fe
DELL User <DELL User@DESKTOP-U4TDB48>
2021-03-15 12:54:53 +0530

    Commit to git local

---
+++ src/test/java/com/vtiger/contact/ContactTest.java
@@ -0,0 +1,24 @@
+package com.vtiger.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+   @Test
+   public void createcontacttest()
+   {
+	   System.out.println("execute create contacttest");
+   }
+   
+   @Test
+   public void modifycontacttest()
+   {
+	   System.out.println("execute modify contacttest");
+   }
+   
+   @Test
+   public void deletecontacttest()
+   {
+	   System.out.println("execute delete contacttest");
+   }
+}
